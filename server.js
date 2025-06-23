7cf512a938fdff0ff7507463a1971b9c2ae43c57
jmodhiambo <jmodhiambo9346@gmail.com>
2025-06-23 08:35:44 +0300

    Task 3. Create a new user

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ import routes from './routes/index.js';
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Enables passing JSON bodies into post request
+app.use(express.json());
+
+// Loads all the API routes
 app.use('/', routes);
 
 app.listen(port, () => {
