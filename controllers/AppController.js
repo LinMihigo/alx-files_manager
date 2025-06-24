c3fe6f5925410cb3d15e7d8156fe2b5e8f99a41e
jmodhiambo <jmodhiambo9346@gmail.com>
2025-06-25 01:41:55 +0300

    Task 3

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,24 +1,19 @@
-// controllers/AppController.js
-import redisClient from '../utils/redis.js';
-import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AppController {
   static getStatus(req, res) {
     res.status(200).json({
       redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
+      db: dbClient.isAlive()
     });
   }
 
   static async getStats(req, res) {
-    try {
-      const users = await dbClient.nbUsers();
-      const files = await dbClient.nbFiles();
-      res.status(200).json({ users, files });
-    } catch (err) {
-      console.error('Error in getStats:', err.message);
-      res.status(500).json({ error: 'Internal server error' });
-    }
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+
+    res.status(200).json({ users, files });
   }
 }
 
--- old_serve.js
+++ old_serve.js
@@ -1,9 +1,14 @@
+// server.js
 import express from 'express';
-import routes from './routes/index';
+import routes from './routes/index.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Enables passing JSON bodies into post request
+app.use(express.json());
+
+// Loads all the API routes
 app.use('/', routes);
 
 app.listen(port, () => {
